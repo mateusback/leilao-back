43cc49d085ba208c67ed30c504dcc0bf193c9dbe
Mateus Back <m.back@nextage.com.br>
2024-09-23 21:21:33 -0300

    first commit

---
+++ src/main/java/com/mateus_back/leilao/controller/IndexController.java
@@ -0,0 +1,14 @@
+﻿package com.mateus_back.leilao.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+public class IndexController {
+    @GetMapping
+    public String index() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/mateus_back/leilao/LeilaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus_back.leilao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeilaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
