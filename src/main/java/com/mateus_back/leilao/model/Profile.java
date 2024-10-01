d0662031909cb4c902d5bdf262d89fc10b176ff1
Mateus Back <m.back@nextage.com.br>
2024-09-30 22:26:09 -0300

    feat: implements person and profile models

---
+++ src/main/java/com/mateus_back/leilao/model/Profile.java
@@ -0,0 +1,17 @@
+package com.mateus_back.leilao.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "profile")
+@Data
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+}
---
+++ src/main/java/com/mateus_back/leilao/service/ProfileService.java
@@ -0,0 +1,37 @@
+package com.mateus_back.leilao.service;
+
+import com.mateus_back.leilao.model.Profile;
+import com.mateus_back.leilao.repository.interfaces.IProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class ProfileService {
+
+    @Autowired
+    private IProfileRepository iProfileRepository;
+
+    public Profile create(Profile profile) {
+        return iProfileRepository.save(profile);
+    }
+
+    public Profile update(Profile profile) {
+        var savedProfile = iProfileRepository.findById(profile.getId())
+                .orElseThrow(() -> new NoSuchElementException("Profile not found"));
+        savedProfile.setName(profile.getName());
+        return iProfileRepository.save(savedProfile);
+    }
+
+    public void delete(Long id) {
+        var savedProfile = iProfileRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Profile not found"));
+        iProfileRepository.deleteById(id);
+    }
+
+    public List<Profile> findAll() {
+        return iProfileRepository.findAll();
+    }
+}
